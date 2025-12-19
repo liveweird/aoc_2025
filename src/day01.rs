0b462cfa3b2df6689772300e317c824697d1bc17
Sebastian Gebski <sebastian@gebski.net>
2025-12-20 00:17:36 +0100

    Day 01 part 2 still doesn't pass

--- src/day01.rs
+++ src/day01.rs
@@ -6,15 +6,30 @@ enum Direction {
     Right,
 }
 
+impl std::fmt::Display for Direction {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Direction::Left => write!(f, "Left"),
+            Direction::Right => write!(f, "Right"),
+        }
+    }
+}
+
 #[derive(PartialEq)]
 struct Operation {
     direction: Direction,
-    distance: i16
+    distance: i16,
+}
+
+impl std::fmt::Display for Operation {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{} {}", self.direction, self.distance)
+    }
 }
 
 struct Dial {
     pos: i16,
-    max: i16
+    max: i16,
 }
 
 impl Dial {
@@ -22,23 +37,36 @@ impl Dial {
         Self { pos, max }
     }
 
-    fn turn(&mut self, operation: Operation) {
+    fn turn(&mut self, operation: &Operation) -> i16 {
+        let mut spills: i16 = 0;
+        let size: i16 = self.max + 1;
         // if the direction is left, subtract the distance from the position
         if operation.direction == Direction::Left {
-            self.pos = (self.max + 1 + self.pos - operation.distance) % (self.max + 1);
+            let increased = self.pos - operation.distance;
+            if self.pos != 0 && increased <= 0 {
+                spills += 1;
+            }
+            spills += (increased / size).abs();
+            self.pos = (size + increased) % size;
         }
         // if the direction is right, add the distance to the position
         else {
-            self.pos = (self.pos + operation.distance) % (self.max + 1);
+            let increased = self.pos + operation.distance;
+            spills += increased / size;
+            self.pos = increased % size;
         }
+        spills
     }
 
-    fn inspect<F>(&self, f: F) -> bool where F: Fn(i16) -> bool {
+    fn inspect<F>(&self, f: F) -> bool
+    where
+        F: Fn(i16) -> bool,
+    {
         f(self.pos)
     }
 }
 
-pub fn day01_part1(filename: &str) -> u64 {
+pub fn day01_part1(filename: &str) -> i16 {
     let mut dial = Dial::new(50, 99);
 
     // create a collection to keep all the operations
@@ -51,22 +79,65 @@ pub fn day01_part1(filename: &str) -> u64 {
     for line in lines {
         // each line is in a format: "DN" where D just the first char and N is a remainder parsed as a number
         let (direction, distance) = line.split_at(1);
-        
+
         if direction == "L" {
-            operations.push(Operation { direction: Direction::Left, distance: distance.parse::<i16>().unwrap() });
+            operations.push(Operation {
+                direction: Direction::Left,
+                distance: distance.parse::<i16>().unwrap(),
+            });
         } else {
-            operations.push(Operation { direction: Direction::Right, distance: distance.parse::<i16>().unwrap() });
+            operations.push(Operation {
+                direction: Direction::Right,
+                distance: distance.parse::<i16>().unwrap(),
+            });
         }
     }
 
     // execute all the operations
     let mut counter = 0;
     for operation in operations {
-        dial.turn(operation);
+        dial.turn(&operation);
         // inspect the position of the dial, if it's zero, increment the counter
         if dial.inspect(|pos| pos == 0) {
             counter += 1;
         }
+    }
+    counter
+}
+
+pub fn day01_part2(filename: &str) -> i16 {
+    let mut dial = Dial::new(50, 99);
+
+    // create a collection to keep all the operations
+    let mut operations = Vec::new();
+
+    // read the input file
+    let input = fs::read_to_string(filename).unwrap();
+    let lines = input.lines();
+
+    for line in lines {
+        // each line is in a format: "DN" where D just the first char and N is a remainder parsed as a number
+        let (direction, distance) = line.split_at(1);
+
+        if direction == "L" {
+            operations.push(Operation {
+                direction: Direction::Left,
+                distance: distance.parse::<i16>().unwrap(),
+            });
+        } else {
+            operations.push(Operation {
+                direction: Direction::Right,
+                distance: distance.parse::<i16>().unwrap(),
+            });
+        }
+    }
+
+    // execute all the operations
+    let mut counter = 0;
+    for operation in operations {
+        let spills = dial.turn(&operation);
+        counter += spills;
+        println!("Position: {}, Operation: {}, Spills: {}", dial.pos, operation, spills);
     }
     counter
 }
@@ -85,5 +156,17 @@ mod tests {
     fn day01_part1_b() {
         let result = day01_part1("input/day01b.txt");
         assert_eq!(result, 1158);
+    }
+
+    #[test]
+    fn day01_part2_a() {
+        let result = day01_part2("input/day01a.txt");
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn day01_part2_b() {
+        let result = day01_part2("input/day01b.txt");
+        assert_eq!(result, 6914);
     }
 }

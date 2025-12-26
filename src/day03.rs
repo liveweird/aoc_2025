136c58acffdb5f7391a6a231b4bfe15bcc4a041d
Sebastian Gebski <sebastian@gebski.net>
2025-12-26 18:08:20 +0100

    Starting Day 03

---
+++ src/day03.rs
@@ -0,0 +1,14 @@
+pub fn day03_part1(filename: &str) -> i64 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day03_part1_a() {
+        let result = day03_part1("input/day03a.txt");
+        assert_eq!(result, 357);
+    }
+}

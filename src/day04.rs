02ba6f282f1d084799a73985ebd06ce73cf1bce6
Sebastian Gebski <sebastian@gebski.net>
2025-12-30 12:11:33 +0100

    Starting work on day 4

---
+++ src/day04.rs
@@ -0,0 +1,14 @@
+pub fn day04_part1(filename: &str) -> u64 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day04_part1_a() {
+        let result = day04_part1("input/day04a.txt");
+        assert_eq!(result, 13);
+    }
+}
